7fc61daa22af2dcc4ba9c87dd3b1d9460f1046b1
DimaPalamarchuk <dimapalamarchuk310@gmail.com>
2025-03-14 18:17:54 +0100

    Add Sweeatalert Notification

--- OrderBlaze/Helper/IJSRuntimeExtensions.cs
+++ OrderBlaze/Helper/IJSRuntimeExtensions.cs
@@ -4,6 +4,8 @@ namespace OrderBlaze.Helper
 {
     public static class IJSRuntimeExtensions
     {
+
+        // For the Toastr
         public static async Task ToastrSuccess(this IJSRuntime jsRuntime, string message)
         {
             await jsRuntime.InvokeVoidAsync("showToastr", "success", message);
@@ -11,6 +13,16 @@ namespace OrderBlaze.Helper
         public static async Task ToastrError(this IJSRuntime jsRuntime, string message)
         {
             await jsRuntime.InvokeVoidAsync("showToastr", "error", message);
+        }
+
+        // For the Sweetalert 
+        public static async Task SweatalertSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("showSwal", "success", message);
+        }
+        public static async Task SweetalertError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("showSwal", "error", message);
         }
     }
 }
